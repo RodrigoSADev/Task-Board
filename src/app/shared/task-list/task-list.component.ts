fccc0ab1d64d46936047f3945a7bcdad621cde59
Rodrigo Silva de Almeida <rodrigosalmeidadev@gmail.com>
2025-01-15 18:23:17 -0300

    feat: update task list component to display tasks with removal functionality

--- src/app/shared/task-list/task-list.component.ts
+++ src/app/shared/task-list/task-list.component.ts
@@ -1,11 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { TaskService } from '../../services/task.service';
 
 @Component({
   selector: 'app-task-list',
-  imports: [],
+  imports: [MatButtonModule],
   templateUrl: './task-list.component.html',
-  styleUrl: './task-list.component.scss'
+  styleUrl: './task-list.component.scss',
 })
 export class TaskListComponent {
+  taskService = inject(TaskService);
 
+  tasks = this.taskService.tasks;
+
+  onRemoveTask(title: string, category: string) {
+    this.taskService.removeTask(title, category);
+  }
 }
