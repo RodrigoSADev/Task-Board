0a645d5d978b95cb36079b43eb49016263686c53
Rodrigo Silva de Almeida <rodrigosalmeidadev@gmail.com>
2025-01-15 18:22:35 -0300

    feat: enhance TaskService with task management functionality and local storage integration

--- src/app/services/task.service.ts
+++ src/app/services/task.service.ts
@@ -1,6 +1,31 @@
-import { Injectable } from '@angular/core';
+import { effect, Injectable, signal } from '@angular/core';
+import { ITask } from '../interfaces/task.interface';
 
 @Injectable({
   providedIn: 'root',
 })
-export class TaskService {}
+export class TaskService {
+  private readonly storageKey = 'tasks';
+  tasks = signal<ITask[]>(this.loadTasks());
+
+  constructor() {
+    effect(() => {
+      localStorage.setItem(this.storageKey, JSON.stringify(this.tasks()));
+    });
+  }
+
+  addTask(newTask: ITask): void {
+    this.tasks.update((tasks) => [...tasks, newTask]);
+  }
+
+  removeTask(title: string, category: string): void {
+    this.tasks.update((tasks) =>
+      tasks.filter((task) => task.title !== title || task.category !== category)
+    );
+  }
+
+  private loadTasks(): ITask[] {
+    const storedTasks = localStorage.getItem(this.storageKey);
+    return storedTasks ? JSON.parse(storedTasks) : [];
+  }
+}
