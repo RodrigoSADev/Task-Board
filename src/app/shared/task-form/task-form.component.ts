c1923c327bb5a92061595c899cd34f6e3eca643a
Rodrigo Silva de Almeida <rodrigosalmeidadev@gmail.com>
2025-01-15 18:23:00 -0300

    feat: implement reactive form for task input with validation and category selection

--- src/app/shared/task-form/task-form.component.ts
+++ src/app/shared/task-form/task-form.component.ts
@@ -1,8 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatSelectModule } from '@angular/material/select';
+import { ITask } from '../../interfaces/task.interface';
+import { TaskService } from '../../services/task.service';
 
 @Component({
   selector: 'app-task-form',
@@ -11,8 +14,50 @@ import { MatSelectModule } from '@angular/material/select';
     MatInputModule,
     MatSelectModule,
     MatButtonModule,
+    ReactiveFormsModule,
   ],
   templateUrl: './task-form.component.html',
   styleUrl: './task-form.component.scss',
 })
-export class TaskFormComponent {}
+export class TaskFormComponent {
+  formBuilder = inject(FormBuilder);
+  taskService = inject(TaskService);
+
+  categories = [
+    {
+      value: 'home',
+      viewValue: 'Casa',
+    },
+    {
+      value: 'work',
+      viewValue: 'Trabalho',
+    },
+    {
+      value: 'study',
+      viewValue: 'Estudo',
+    },
+    {
+      value: 'personal',
+      viewValue: 'Pessoal',
+    },
+    {
+      value: 'other',
+      viewValue: 'Outros',
+    },
+  ];
+  taskForm = this.formBuilder.group({
+    title: ['', [Validators.required]],
+    category: ['', [Validators.required]],
+  });
+
+  onAddTask() {
+    if (this.taskForm.valid) {
+      const newTask = {
+        title: this.taskForm.value.title || '',
+        category: this.taskForm.value.category || '',
+      } as ITask;
+      this.taskService.addTask(newTask);
+      this.taskForm.reset();
+    }
+  }
+}
