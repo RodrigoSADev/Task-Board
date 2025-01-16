00dccea0701fefb9071c7fca79fa729e5dfce5ff
Rodrigo Silva de Almeida <rodrigosalmeidadev@gmail.com>
2025-01-16 17:56:24 -0300

    feat: add edit task functionality to task list component

--- src/app/shared/task-list/task-list.component.ts
+++ src/app/shared/task-list/task-list.component.ts
@@ -1,6 +1,9 @@
 import { Component, inject } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
+import { MatDialog } from '@angular/material/dialog';
+import { ITask } from '../../interfaces/task.interface';
 import { TaskService } from '../../services/task.service';
+import { EditTaskDialogComponent } from '../edit-task-dialog/edit-task-dialog.component';
 
 @Component({
   selector: 'app-task-list',
@@ -10,6 +13,7 @@ import { TaskService } from '../../services/task.service';
 })
 export class TaskListComponent {
   taskService = inject(TaskService);
+  dialog = inject(MatDialog);
 
   tasks = this.taskService.tasks;
 
@@ -17,7 +21,20 @@ export class TaskListComponent {
     this.taskService.toggleTaskCompletion(title, category);
   }
 
-  onRemoveTask(title: string, category: string) {
+  onEditTask(task: ITask): void {
+    const dialogRef = this.dialog.open(EditTaskDialogComponent, {
+      width: '400px',
+      data: { ...task },
+    });
+
+    dialogRef.afterClosed().subscribe((result: ITask) => {
+      if (result) {
+        this.taskService.updateTask(task.title, task.category, result);
+      }
+    });
+  }
+
+  onRemoveTask(title: string, category: string): void {
     this.taskService.removeTask(title, category);
   }
 }
