4bfa9daaa35ae0fa09f52c817212d56ba82c4a00
Rodrigo Silva de Almeida <rodrigosalmeidadev@gmail.com>
2025-02-20 13:53:07 -0300

    refactor: reorganize project structure

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
-import { TaskFormComponent } from './shared/task-form/task-form.component';
-import { TaskListComponent } from './shared/task-list/task-list.component';
+import { TaskFormComponent } from './components/task-form/task-form.component';
+import { TaskListComponent } from './components/task-list/task-list.component';
 
 @Component({
   selector: 'app-root',
